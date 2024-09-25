dbffa770968c410bdf3ab9be55eec15a2e4ebc52
magmafellow <magmafellow@gmail.com>
2024-09-25 15:21:52 +0300

    redirect from root route via router

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+
 import { permanentRedirect } from "next/navigation"
+import { useRouter } from "next/router"
+import { useEffect } from 'react'
 
 export default function Page() {
-  permanentRedirect('/feed')
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/feed')
+  })
   return (
     <div>redirected</div>
   )
